c4a7401902a5699e6a6bd912346bae6d359f8221
Cowboy Lynk <clynk@mit.edu>
2019-03-02 10:33:09 -0500

    Initial Commit

---
+++ Alcoholics Identified/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alcoholics Identified
+//
+//  Created by Cowboy Lynk on 3/2/19.
+//  Copyright © 2019 Lampshade Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
